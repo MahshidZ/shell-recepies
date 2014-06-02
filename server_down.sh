996aa006f1d3e50b9eea54964445df32f35612e0
Mahshid Zeinaly <mahshid.zeinaly@gmail.com>
2014-06-02 13:20:22 -0700

    Broadcasting a message is implemented

---
+++ server_down.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#-------------------------------------------
+# Write a script to ask the user whether the
+# server is working or not. And if it is not
+# working, inform all the connected users on
+# the server
+#-------------------------------------------
+
+clear
+
+echo "Is the server working?"
+select answer in Yes No Exit
+do
+  case $answer in
+    Yes)
+      echo Server is going to be down. Please save your work | wall;;
+    No)
+      echo "Good!";;
+    Exit)
+      exit;;
+    *) 
+      echo "$answer is an invalid option"
+  esac
+done
+
+
+
