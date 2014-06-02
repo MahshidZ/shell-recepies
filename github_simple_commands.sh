456482347b10462545b843ab44ae448a603874f6
Mahshid Zeinaly <mahshid.zeinaly@gmail.com>
2014-06-02 15:14:38 -0700

    A simple script to add to github is added

--- github_simple_commands.sh
+++ github_simple_commands.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#------------------------------------------------------
+# To run type chmod +x github_simple_commands.sh
+# and run ./github_simple_commands "my commit message"
+#-------------------------------------------------------
+clear
 
 git add .
 git commit -m "$1"
